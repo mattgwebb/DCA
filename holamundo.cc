a0fbd97c23d1fe165a3c45d8f3e0faa9a1c88e98
matt <matt@ua.es>
2015-12-22 13:36:09 +0100

    holamundo modificado, anyadido git

--- holamundo.cc
+++ holamundo.cc
@@ -3,4 +3,5 @@ int main() {
 	std::cout << "Hola Mundo" << std::endl;
 	std::cout << "Practica de DCA" << std::endl;
 	std::cou << "21/12/2015" << std::endl;
+	std::cout << "Git" << std::endl;
 	return 1; }
