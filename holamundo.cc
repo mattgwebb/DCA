e187e5060bf47c72b69376e44eebd60a0a155542
matt <matt@ua.es>
2015-12-22 13:35:15 +0100

    holamundo modificado, saltos de linea

--- holamundo.cc
+++ holamundo.cc
@@ -1,6 +1,6 @@
 #include <iostream>
 int main() { 
-	std::cout << "Hola Mundo";
-	std::cout << "Practica de DCA";
-	std::cou << "21/12/2015";
+	std::cout << "Hola Mundo" << std::endl;
+	std::cout << "Practica de DCA" << std::endl;
+	std::cou << "21/12/2015" << std::endl;
 	return 1; }
